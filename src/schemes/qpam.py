e5c0db3d839edcbcec3583249e6a2cb45d80031e
Vignesh S <74590303+stringtherapy@users.noreply.github.com>
2024-03-28 17:23:38 +0000

    removed-constant-dependency

--- src/schemes/qpam.py
+++ src/schemes/qpam.py
@@ -5,13 +5,12 @@ import numpy as np
 class QPAM:
 	def __init__(self):
 		self.name = 'Quantum Probability Amplitude Modulation'
-		self.norm = None
 
 	def encode(self,array):
-		self.norm,amplitudes = utils.convert_to_probability_amplitudes(array)
+		norm,amplitudes = utils.convert_to_probability_amplitudes(array)
 		time_register_size = int(np.log2(len(array)))
 		time_register = qiskit.QuantumRegister(time_register_size,'t')
-		qc = qiskit.QuantumCircuit(time_register)
+		qc = qiskit.QuantumCircuit(time_register,metadata={'norm_factor':norm})
 		qc.initialize(amplitudes)
 		qc.measure_all()
 		return qc
@@ -21,6 +20,7 @@ class QPAM:
 		result = job.result()
 		counts = utils.pad_counts(result.get_counts())
 		probabilities = np.array(list(counts.values()))
-		return 2*self.norm*np.sqrt(probabilities/shots)-1
+		norm = qc.metadata['norm_factor']
+		return 2*norm*np.sqrt(probabilities/shots)-1
 
 
