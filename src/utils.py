b92c90586efd53d4c75b9fd58bfb12c659b91a78
Vignesh S <74590303+stringtherapy@users.noreply.github.com>
2024-05-05 12:09:02 +0100

    organised-utils

--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,8 @@ from PIL import Image
 from IPython.display import display, Audio, clear_output
 import pyaudio
 
+# Data processing utils
+
 # Create synthetic data
 def simulate_data(num_samples,num_channels=1,seed=42):
 	np.random.seed(seed)
@@ -15,13 +17,19 @@ def simulate_data(num_samples,num_channels=1,seed=42):
 	if num_channels == 1: data = data.squeeze()
 	return data
 
-def create_circuit(dimensions,metadata,name=('t','a')): #adapt to L and R
-	index_register = qiskit.QuantumRegister(dimensions[0],name[0])
-	value_registers = []
-	for channel in dimensions[1]:
-		value_registers.append(qiskit.QuantumRegister(channel,name[1]))
-	qc = qiskit.QuantumCircuit(*value_registers,index_register,metadata=metadata)
-	return qc
+def apply_padding(array,time_resolution):
+	pad_length = 2**time_resolution-len(array)
+	if pad_length: array = np.pad(array,(0,pad_length))
+	return array
+
+def get_bit_depth(signal):
+    unique_values = set(signal)
+    num_levels = len(unique_values)
+    bit_depth = np.ceil(np.log2(num_levels))
+    if not bit_depth: bit_depth = 1
+    return int(bit_depth)
+
+# Conversions
 
 def convert_to_probability_amplitudes(array):
 	array = (array.astype(float)+1)/2
@@ -32,21 +40,11 @@ def convert_to_probability_amplitudes(array):
 def convert_to_angles(array):
 	return np.arcsin(np.sqrt((array.astype(float)+1)/2))
 
+# Quantum Utils
+
 def get_qubit_count(data_length):
 	num_qubits = int(np.ceil(np.log2(data_length)))
 	return num_qubits
-
-def apply_padding(array,time_resolution):
-	pad_length = 2**time_resolution-len(array)
-	if pad_length: array = np.pad(array,(0,pad_length))
-	return array  
-
-def get_bit_depth(signal):
-    unique_values = set(signal)
-    num_levels = len(unique_values)
-    bit_depth = np.ceil(np.log2(num_levels))
-    if not bit_depth: bit_depth = 1
-    return int(bit_depth)
 
 def pad_counts(counts):
 	num_qubits = len(next(iter(counts)))
@@ -92,6 +90,8 @@ def measure(qc,treg_pos = 1,areg_pos = 0,labels=('ca','ct')):
 	qc.measure(treg, ctreg)
 	qc.measure(areg, careg)
 
+# Plotting utils
+
 def plot(samples):
 	if type(samples) != list: samples = [samples]
 	
@@ -103,7 +103,6 @@ def plot(samples):
 
 	plt.xlabel("Index")
 	plt.ylabel("Values")
-	#plt.legend()
 	plt.show()
 
 def tune(obj,function,max_value=2048,step=10,name='Shots',ref=None,limit=None):
@@ -122,16 +121,6 @@ def tune(obj,function,max_value=2048,step=10,name='Shots',ref=None,limit=None):
 	variable_slider = ipywidgets.IntSlider(value=1, min=1, max=max_value, step=step, description=name)
 	return ipywidgets.interact(plot_function, shots=variable_slider)
 
-def tune_img(obj,function,max_value=2048,step=10,name='Shots',size=25,upscale=2,limit=None):
-	def plot_function(shots):
-		y = function(qc=obj,backend=None,shots=shots)
-		y = y[:int(size*size)]
-		clear_output(wait=True)
-		display(array_to_image(y,size=size,upscale=upscale))
-		play(interpolate(y),autoplay=True)
-	variable_slider = ipywidgets.IntSlider(value=1, min=1, max=max_value, step=step, description=name)
-	return ipywidgets.interact(plot_function, shots=variable_slider)
-
 def interpolate(samples,step_size=0.025,kind='linear'):
     num_samples = len(samples)
     x = np.arange(0,num_samples)
@@ -139,23 +128,8 @@ def interpolate(samples,step_size=0.025,kind='linear'):
     f = scipy.interpolate.interp1d(x,y,kind=kind)
     x_new = np.arange(0,num_samples-1,step_size)
     y_new = f(x_new)
-    #print(f'Interpolated number of samples from: {num_samples} to {len(y_new)}')
+    print(f'Interpolated number of samples from: {num_samples} to {len(y_new)}')
     return y_new/2
-
-def image_to_array(image_path,size=100,mode='L'):
-	img = Image.open(image_path).resize((size, size))
-	img_gray = img.convert(mode)
-	img_array = np.array(img_gray)
-	img_1d = img_array.flatten()
-	return img_1d
-
-def array_to_image(array,size=100,mode='L',upscale=None):
-	array = array.reshape((size, size))
-	dtype = 'bool' if mode == '1' else 'uint8' 
-	img = Image.fromarray(array.astype(dtype), mode)
-	if upscale:
-		img = img.resize((size*upscale,size*upscale), Image.LANCZOS)
-	return img
 
 def play(array,rate=44100,autoplay=False):
 	audio = Audio(data=array,rate=rate,autoplay=autoplay)
