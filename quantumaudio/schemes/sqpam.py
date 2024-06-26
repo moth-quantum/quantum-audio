18ceb8a568173db5b99047b01aaae5b11ebef1c0
Vignesh S <74590303+stringtherapy@users.noreply.github.com>
2024-06-26 16:55:48 +0100

    SPQAM - cleaned

--- quantumaudio/schemes/sqpam.py
+++ quantumaudio/schemes/sqpam.py
@@ -6,46 +6,48 @@ class SQPAM:
 	def __init__(self):
 		self.name = 'Single-Qubit Probability Amplitude Modulation'
 		self.qubit_depth = 1
-		self.labels      = ('time','channel','amplitude')
+		self.labels = ('time','amplitude')
 
-	def encode(self,data):
+	def encode(self,data,measure=True,verbose=2):
 		# x-axis
 		num_samples      = data.shape[-1]
 		num_index_qubits = utils.get_qubit_count(num_samples)
 		
 		# y-axis
-		num_channels     = 1 if data.ndim == 1 else data.shape[0]
-		num_channel_qubits = utils.get_qubit_count(num_channels)
+		assert data.ndim == 1 or data.shape[0] == 1, "Multi-channel not supported in SQPAM"
 		num_value_qubits = self.qubit_depth
 
+		# print
+		if verbose:
+			print(f'Number of qubits required: {num_index_qubits+num_value_qubits}\n')
+			print(f'{num_index_qubits} for {self.labels[0]}')
+			print(f'{num_value_qubits} for {self.labels[1]}\n')
+
 		# prepare data
-		data   = utils.apply_index_padding(data,num_index_qubits)
-		values = utils.convert_to_angles(data)
+		data = utils.apply_index_padding(data,num_index_qubits)
+		values = utils.convert_to_angles(data).squeeze()
 
 		# prepare circuit
-		index_register   = qiskit.QuantumRegister(num_index_qubits,self.labels[0])
-		channel_register = qiskit.QuantumRegister(num_channel_qubits,self.labels[1])
-		value_register   = qiskit.QuantumRegister(num_value_qubits,self.labels[2])
-
-		circuit = qiskit.QuantumCircuit(value_register,channel_register,index_register)
+		index_register = qiskit.QuantumRegister(num_index_qubits,self.labels[0])
+		value_register = qiskit.QuantumRegister(num_value_qubits,self.labels[1])
+		circuit = qiskit.QuantumCircuit(value_register,index_register)
 		circuit.h(index_register)
 		
 		# encode values
-		values = values.squeeze() if num_channels == 1 else utils.interleave_channels(values)
 		for i, value in enumerate(values):        
 			self.value_setting(circuit=circuit, index=i, value=value)
 		
 		# additional information for decoding
 		circuit.metadata = {'num_samples':num_samples}
 
-		# measure
-		utils.measure(circuit)
-
+		# measure, print and return
+		if measure: self.measure(circuit)
+		if verbose == 2: utils.draw_circuit(circuit,decompose=1)
 		return circuit
 
 	@utils.with_indexing
 	def value_setting(self,circuit,index,value):
-		value_register, channel_register, index_register = circuit.qregs
+		value_register, index_register = circuit.qregs
 		
 		# initialise sub-circuit
 		sub_circuit = qiskit.QuantumCircuit(name=f'Sample {index}')
@@ -56,30 +58,29 @@ class SQPAM:
 			sub_circuit.ry(2*value, i)
 
 		# entangle with index qubits
-		sub_circuit = sub_circuit.control(channel_register.size + index_register.size)
+		sub_circuit = sub_circuit.control(index_register.size)
 		
 		# attach sub-circuit
 		circuit.append(sub_circuit, [i for i in range(circuit.num_qubits-1,-1,-1)])
 
-	def decode(self,circuit,backend=None,shots=1024,inverted=False):
+	def measure(self,circuit):
+		if not circuit.cregs: utils.measure(circuit)
+
+	def decode(self,circuit,backend=None,shots=1024,inverted=False,keep_padding=False):
 		# execute
+		self.measure(circuit)
 		counts = utils.get_counts(circuit=circuit,backend=backend,shots=shots)
 		
 		# decoding x-axis
-		num_channel_qubits = circuit.qregs[1].size
-		num_index_qubits   = circuit.qregs[2].size
-
+		num_index_qubits = circuit.qregs[1].size
 		num_samples = 2 ** num_index_qubits
-		num_channels = 2 ** num_channel_qubits
-
-		total_samples = num_samples*num_channels
-		original_num_samples = circuit.metadata['num_samples']*num_channels
+		original_num_samples = circuit.metadata['num_samples']
 
 		# decoding y-axis
-		
+
 		# initialising components
-		cosine_amps = np.zeros(total_samples)
-		sine_amps   = np.zeros(total_samples)
+		cosine_amps = np.zeros(num_samples)
+		sine_amps   = np.zeros(num_samples)
 
 		# getting components from counts
 		for state in counts:
@@ -93,11 +94,13 @@ class SQPAM:
 
 		total_amps = cosine_amps+sine_amps
 		amps = sine_amps if not inverted else cosine_amps
+		
+		# reconstruct
 		ratio = np.divide(amps, total_amps, out=np.zeros_like(amps), where=total_amps!=0)
 		data = 2 * (ratio) - 1
 
-		data = data[:original_num_samples]
-		if num_channels > 1: data = utils.restore_channels(data,num_channels)
+		# undo padding
+		if not keep_padding: 
+			data = data[:original_num_samples]
 
 		return data
-
