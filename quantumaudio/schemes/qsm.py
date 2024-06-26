11bcb6ebcf864039a5407606ac60c6b4b9bb0a09
Vignesh S <74590303+stringtherapy@users.noreply.github.com>
2024-06-26 17:16:43 +0100

    QSM cleaned

--- quantumaudio/schemes/qsm.py
+++ quantumaudio/schemes/qsm.py
@@ -9,33 +9,41 @@ class QSM:
 		self.qubit_depth = qubit_depth
 		self.labels = ('time','amplitude')
 
-	def encode(self,data):
+	def encode(self,data,measure=True,verbose=2):
 		# x-axis
-		num_samples      = data.shape[-1]
+		num_samples = data.shape[-1]
 		num_index_qubits = utils.get_qubit_count(num_samples)
 		
 		# y-axis
-		num_channels     = 1 if data.ndim == 1 else data.shape[0]
+		assert data.ndim == 1 or data.shape[0] == 1, "Multi-channel not supported in QSM"
 		num_value_qubits = utils.get_bit_depth(data) if not self.qubit_depth else self.qubit_depth
+
+		# print
+		if verbose:
+			print(f'Number of qubits required: {num_index_qubits+num_value_qubits}\n')
+			print(f'{num_index_qubits} for {self.labels[0]}')
+			print(f'{num_value_qubits} for {self.labels[1]}\n')
 		
 		# prepare data
-		data   = utils.apply_index_padding(data,num_index_qubits)
-		values = utils.quantize(data,num_value_qubits)
+		data = utils.apply_index_padding(data,num_index_qubits)
+		values = utils.quantize(data,num_value_qubits).squeeze()
 
 		# prepare circuit
 		index_register 		= qiskit.QuantumRegister(num_index_qubits,self.labels[0])
-		amplitude_register 	= qiskit.QuantumRegister(num_value_qubits,self.labels[-1])
+		amplitude_register 	= qiskit.QuantumRegister(num_value_qubits,self.labels[1])
 		circuit = qiskit.QuantumCircuit(amplitude_register,index_register)
 		circuit.h(index_register)
 
-		# encode information
-		values = values.squeeze()
+		# encode values
 		for i, sample in enumerate(values):
 			self.value_setting(circuit=circuit, index=i, value=sample)
 
-		# measure
-		utils.measure(circuit)
+		# additional information for decoding
+		circuit.metadata = {'num_samples':num_samples}
 
+		# measure, print and return
+		if measure: self.measure(circuit)
+		if verbose == 2: utils.draw_circuit(circuit)
 		return circuit
 
 	@utils.with_indexing
@@ -48,26 +56,35 @@ class QSM:
 			if a_bit:
 				circuit.mct(treg, areg_qubit)
 
+	def measure(self,circuit):
+		if not circuit.cregs: utils.measure(circuit)
 
-	def decode(self,circuit,backend=None,shots=4000):
+	def decode(self,circuit,backend=None,shots=4000,keep_padding=False):
 		# execute
 		counts = utils.get_counts(circuit=circuit,backend=backend,shots=shots,pad=False)
 
 		# decoding x-axis
 		num_index_qubits = circuit.qregs[1].size
 		num_samples = 2 ** num_index_qubits
+		original_num_samples = circuit.metadata['num_samples']
 		
 		# decoding y-axis
 		bit_depth = circuit.qregs[0].size
 
-		# decoding data
+		# initialising data
 		data = np.zeros(num_samples, int)
 
+		# decoding data
 		for state in counts:
 			(t_bits, a_bits) = state.split()
 			t = int(t_bits, 2)
 			a = BitArray(bin=a_bits).int
 			data[t] = a
 
+		# reconstruct
 		data = data/(2**(bit_depth-1))
+
+		# undo padding
+		if not keep_padding: 
+			data = data[:original_num_samples]
 		return data
