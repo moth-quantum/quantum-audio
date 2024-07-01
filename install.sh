3f577e817cce309cd682124d1773102c4b85be95
Vignesh S <74590303+stringtherapy@users.noreply.github.com>
2024-07-02 01:11:40 +0530

    cleaned-non-API-files

--- install.sh
+++
@@ -1,6 +0,0 @@
-env_name="qa"
-source ~/miniconda3/bin/activate
-conda create --name $env_name python=3.10 -y
-
-conda activate $env_name
-pip install -r requirements.txt
--- miniconda.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p ~/miniconda3
-curl https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-arm64.sh -o ~/miniconda3/miniconda.sh
-bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
-rm -rf ~/miniconda3/miniconda.sh
-~/miniconda3/bin/conda init zsh
