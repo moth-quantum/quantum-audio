8234de81176d24ec2a02231b32326e8a43e26ada
Vignesh S <74590303+stringtherapy@users.noreply.github.com>
2024-07-02 12:22:12 +0530

    Delete main.py

--- quantumaudio/main.py
+++
@@ -1,8 +0,0 @@
-import importlib
-  
-def load(name,module_name='quantumaudio.schemes'):
-    try:
-        scheme = importlib.import_module(f'{module_name}.{name.lower()}')
-        return getattr(scheme, name.upper())()
-    except (ImportError, AttributeError) as e:
-        raise ImportError(f"Could not load class '{name}' from module '{module_name}'. Error: {e}")
