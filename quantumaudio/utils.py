b6f0fbd7f641cfad32c0fdc4843fc31f2769dae1
Vignesh S <74590303+stringtherapy@users.noreply.github.com>
2024-07-04 12:32:37 +0530

    Update utils.py

--- quantumaudio/utils.py
+++ quantumaudio/utils.py
@@ -14,7 +14,9 @@ from typing import Union, Callable, Optional, Any
 
 
 def simulate_data(num_samples: int, num_channels: int = 1, seed: int = 42) -> np.ndarray:
-    """Simulates random data.
+    """Simulates sythetic data for quick testing and plots. Typically, Audio data
+       contains several thousands of samples per second which is difficult to 
+       visualise as circuit and plot" 
 
     Args:
         num_samples (int): The number of samples to generate.
@@ -22,7 +24,7 @@ def simulate_data(num_samples: int, num_channels: int = 1, seed: int = 42) -> np
         seed (int, optional): The seed for the random number generator. Defaults to 42.
 
     Returns:
-        np.ndarray: A numpy array of simulated data. If `num_channels` is 1, the array will be 1-dimensional.
+        np.ndarray: A numpy array of simulated data.
     """
     np.random.seed(seed)
     data = np.random.rand(num_samples, num_channels)
