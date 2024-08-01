34b96ce3fb27024eb98b3d1ad71d55f77d305484
Vignesh S <74590303+stringtherapy@users.noreply.github.com>
2024-08-01 14:02:13 +0100

    Update __init__.py
    progress

--- quantumaudio/utils/__init__.py
+++ quantumaudio/utils/__init__.py
@@ -1,3 +1,13 @@
+"""
+This subpackages contains utilary functions that support the 
+encoding and decoding schemes
+
+- data preparation and calculation utils
+- convert the pre-processing stage required for 
+
+"""
+
+
 from .data import *
 from .convert import *
 from .quantum import *
