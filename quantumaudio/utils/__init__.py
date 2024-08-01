34353ae4d20fb8c2b9e5101d9093dd93722ce133
Vignesh S <74590303+stringtherapy@users.noreply.github.com>
2024-08-01 17:39:59 +0100

    Update __init__.py
    Added documentation to utils

--- quantumaudio/utils/__init__.py
+++ quantumaudio/utils/__init__.py
@@ -1,13 +1,16 @@
 """
-This subpackages contains utilary functions that support the 
+This subpackages contains the following utilary modules that support the 
 encoding and decoding schemes
 
-- data preparation and calculation utils
-- convert the pre-processing stage required for 
+- data.py : data preparation and calculation functions
+- convert.py : data pre-processing functions required for encoding values into the quantum circuit
+- circuit.py : helper functions for circuit preparation with qiskit 
+- qiskit_helpers.py : common helper functions for circuit measurements and preview information on the circuit.
 
 """
 
 
 from .data import *
 from .convert import *
-from .quantum import *
+from .circuit import *
+from .qiskit_helpers import *
