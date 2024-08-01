d750f38db283ac2e3eb998baccb3676e187720f0
Vignesh S <74590303+stringtherapy@users.noreply.github.com>
2024-08-01 11:30:06 +0100

    renamed

---
+++ quantumaudio/utils/circuit.py
@@ -0,0 +1,91 @@
+import qiskit
+from typing import Union, Callable, Optional
+
+# ======================
+# Circuit Preparation Utils
+# ======================
+
+def apply_x_at_index(qc: qiskit.QuantumCircuit, i: int) -> None:
+    """This function is used to encode an index as control qubits to a circuit.
+
+    Args:
+        circuit: Qiskit Circuit
+        index: Index position
+    """
+    if len(qc.qregs) != 2:
+        _, creg, treg = qc.qregs
+    else:
+        _, treg = qc.qregs
+        creg = []
+    bitstring = []
+    for reg_index, reg_qubit in enumerate(creg[:] + treg[:]):
+        bit = (i >> reg_index) & 1
+        bitstring.append(bit)
+        if not bit:
+            qc.x(reg_qubit)
+
+
+def with_indexing(func: Callable) -> Callable:
+    """Used as decorator with a value-setting operation.
+
+    Args:
+        func (Callable): A value-setting function to be decorated.
+
+    Returns:
+        Callable: The wrapped function with time indexing applied.
+    """
+
+    def wrapper(*args, **kwargs):
+        qc = kwargs.get("circuit")
+        i = kwargs.get("index")
+        qc.barrier()
+        apply_x_at_index(qc, i)
+        func(*args, **kwargs)
+        apply_x_at_index(qc, i)
+
+    return wrapper
+
+
+def add_classical_register(
+    qc: qiskit.QuantumCircuit, position: int, label: str
+) -> None:
+    """Adds a classical register to a quantum circuit and measures the
+    corresponding quantum register.
+
+    Args:
+        qc (QuantumCircuit): The quantum circuit to which the classical register will be added.
+        position (int): The position of the quantum register in the quantum circuit.
+        label (str): The label for the new classical register.
+
+    Returns:
+        None
+    """
+    qreg = qc.qregs[position]
+    creg = qiskit.ClassicalRegister(qreg.size, label)
+    qc.add_register(creg)
+    qc.measure(qreg, creg)
+
+
+def measure(
+    qc: qiskit.QuantumCircuit,
+    labels: tuple[str, str, str] = ("ca", "cc", "ct"),
+    position: Optional[int] = None,
+) -> None:
+    """Adds a barrier to the quantum circuit and classical registers at the
+    corresponding quantum registers.
+
+    Args:
+        qc (QuantumCircuit): The quantum circuit to which the barrier and measurements will be applied.
+        labels (tuple[str, str, str], optional): Labels for the classical registers corresponding to the measurements.
+                                                 Defaults to ("ca", "cc", "ct").
+        position (int, optional): The position of the qubits to be measured. If None, all qubits are measured. Defaults to None.
+
+    Returns:
+        None
+    """
+    value_pos, *index_pos = range(len(qc.qregs)) if not position else reversed(position)
+    value_label, *index_labels = labels
+    index_labels = index_labels[len(index_labels) - len(index_pos) :]
+    add_classical_register(qc, value_pos, value_label)
+    for i, pos in enumerate(index_pos):
+        add_classical_register(qc, pos, index_labels[i])
