1619d03ef1c48e468f8273f38103ef6c65b28099
hari <74590303+stringtherapy@users.noreply.github.com>
2024-08-14 15:35:55 +0100

    Update __init__.py
    improved tools importing

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,2 +1,14 @@
+import importlib
 from .plot import plot
-from . import audio
+
+def __getattr__(name):
+    try:
+        module = importlib.import_module(f".{name.lower()}", package=__name__)
+        return module
+    except (ImportError, AttributeError) as e:
+        raise AttributeError(f"module {__name__} has no attribute {name}") from e
+
+def __dir__():
+    return __all__
+
+__all__ = ["audio", "interactive", "plot", "stream"]
