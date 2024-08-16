ece00c05681c2343c747687122c2d0aad8f17bf5
hari <74590303+stringtherapy@users.noreply.github.com>
2024-08-16 12:37:43 +0100

    PEP-8 format

--- quantumaudio/schemes/__init__.py
+++ quantumaudio/schemes/__init__.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# ==============================================================================
+# ==========================================================================
 
 """This subpackage contains different schemes. The common information for using any scheme is detailed below.
 
