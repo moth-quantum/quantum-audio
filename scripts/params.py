8cc55cca09ad6d751c9cb60aa029ec06146f7997
hari <74590303+stringtherapy@users.noreply.github.com>
2024-08-16 12:09:45 +0100

    PEP-8 correction
    updated line-length

--- scripts/params.py
+++ scripts/params.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# ==============================================================================
+# ==========================================================================
 
 all_schemes = ['qpam','spqam','qsm','msqpam','mqsm']
 default_scheme = 'qpam'
