1f43ea3c9c7357bc5b39302c107210415e35caa3
hari <74590303+stringtherapy@users.noreply.github.com>
2024-09-27 00:07:14 +0100

    moved-internal-utils

---
+++ quantumaudio/tools/__init__.py
@@ -0,0 +1,30 @@
+from .stream import stream_data
+from .plot import plot
+
+from typing import Optional
+import numpy as np
+np.random.seed(42)
+
+def test_signal(
+    num_channels: int = 1, num_samples: int = 8, seed: Optional[int] = None
+) -> np.ndarray:
+    """Simulates sythetic data for quick testing and plots. Typically, Audio data
+       contains several thousands of samples per second which is difficult to
+       visualise as circuit and plot"
+
+    Args:
+        num_channels: The number of channels for each sample. Defaults to 1.
+        num_samples: The number of samples to generate.
+        seed: The seed for the random number generator. Defaults to 42.
+
+    Returns:
+        A numpy array of simulated data.
+    """
+    if seed: np.random.seed(seed)
+    data = np.random.rand(num_channels, num_samples)
+    data = 2.0 * data - 1.0
+    if num_channels == 1:
+        data = data.squeeze()
+    return data
+
+__all__ = ["plot", "stream", "stream_data", "test_signal"]
--- quantumaudio/utils/data.py
+++ quantumaudio/utils/data.py
@@ -19,30 +19,6 @@ import numpy as np
 # Data processing utils
 # ======================
 
-
-def test_signal(
-    num_channels: int = 1, num_samples: int = 8, seed: int = 42
-) -> np.ndarray:
-    """Simulates sythetic data for quick testing and plots. Typically, Audio data
-       contains several thousands of samples per second which is difficult to
-       visualise as circuit and plot"
-
-    Args:
-        num_channels: The number of channels for each sample. Defaults to 1.
-        num_samples: The number of samples to generate.
-        seed: The seed for the random number generator. Defaults to 42.
-
-    Returns:
-        A numpy array of simulated data.
-    """
-    np.random.seed(seed)
-    data = np.random.rand(num_channels, num_samples)
-    data = 2.0 * data - 1.0
-    if num_channels == 1:
-        data = data.squeeze()
-    return data
-
-
 def apply_index_padding(
     array: np.ndarray, num_index_qubits: int
 ) -> np.ndarray:
