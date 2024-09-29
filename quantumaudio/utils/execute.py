ba218b9499c76f82b15542c9243056b25d93fe48
hari <74590303+stringtherapy@users.noreply.github.com>
2024-09-29 22:06:55 +0100

    added-optional-execute
    A simple SamplerV2 option for execution

--- quantumaudio/utils/execute.py
+++ quantumaudio/utils/execute.py
@@ -1,8 +1,10 @@
 import qiskit_aer
 from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
 from typing import Type, Any
+import importlib
 
 _default_backend = qiskit_aer.AerSimulator()
+_Sampler = getattr(importlib.import_module("qiskit_ibm_runtime"), "SamplerV2", None) if importlib.util.find_spec("qiskit_ibm_runtime") else None
 
 # ---- Default Execute Function ----
 
@@ -28,12 +30,41 @@ def execute(circuit, shots=4000, backend=None, keep_memory=False, optimization_l
     result = job.result()
     return result
 
+# ---- Optional Execute Function ----
+
+def execute_with_sampler(circuit, backend=None, shots=4000, optimization_level=3):
+    assert _Sampler, "IBM runtime is not installed to use Sampler. It can be installed using `pip install qiskit-ibm-runtime`"
+    if not isinstance(circuit, list): circuit = [circuit]
+
+    backend = _default_backend if not backend else backend
+    sampler = _load_instance(_Sampler, mode=backend)
+    
+    transpiler = _load_instance(generate_preset_pass_manager,backend=backend,optimization_level=optimization_level)
+    transpiled_circuit = transpiler.run(circuit) 
+    
+    job = sampler.run(transpiled_circuit, shots=shots)
+    result = job.result()
+
+    # Manually pass circuit metadata for `decode_result` method to use when no metadata is passed explicity.
+    if not result.metadata and hasattr(result, '_metadata'):
+        result._metadata.update(circuit[0].metadata)
+    return result
 
 # ---- Helper Functions ----
 
 _cache = {}
 
 def _load_instance(cls: Type, **kwargs: Any) -> Any:
+    """Load a instance with specified keyword arguments, caching the result to 
+    avoid repeated loading for the same parameters especially during `stream` operation.
+
+    Args:
+        class (Type): Can be Sampler or Preset Pass Manager object.
+        kwargs (dict): Dictionary containing keyword arguments.
+
+    Returns:
+        The loaded or cached object.
+    """
     cache_key = (cls, frozenset(kwargs.items()))
     if cache_key not in _cache:
         _cache[cache_key] = cls(**kwargs)
