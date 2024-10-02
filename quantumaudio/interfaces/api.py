57438aa90a8f42cb24d445b6f6ce95950269002f
hari <74590303+stringtherapy@users.noreply.github.com>
2024-10-02 17:49:27 +0100

    added-type-hinting

--- quantumaudio/interfaces/api.py
+++ quantumaudio/interfaces/api.py
@@ -2,16 +2,17 @@ import quantumaudio
 from quantumaudio import load_scheme
 from quantumaudio.utils import pick_key
 from quantumaudio.tools import stream_data
+from typing import Union
 
 # ------------------- Core Functions ---------------------------
 
-def encode(data, scheme="qpam", **kwargs):
+def encode(data: 'np.ndarray', scheme: Union[str, quantumaudio.schemes.Scheme] = "qpam", **kwargs):
     """Encodes data using a specified quantum scheme.
 
     Args:
-        data (any): The data to encode.
-        scheme (str): Name of the encoding scheme to use. Defaults to "qpam".
-        **kwargs: Additional keyword arguments passed to the encoding method.
+        data: The data to encode.
+        scheme: Name of the encoding scheme or a scheme object to use. Defaults to "qpam".
+        **kwargs: Additional keyword arguments passed required for encoding method and scheme initialisation.
 
     Returns:
         Encoded data according to the provided scheme.
@@ -20,12 +21,12 @@ def encode(data, scheme="qpam", **kwargs):
     return _load_scheme(scheme, **scheme_kwargs).encode(data, **kwargs)
 
 
-def decode(circuit, **kwargs):
+def decode(circuit: 'qiskit.QuantumCircuit', **kwargs):
     """Decodes a quantum circuit using the scheme it was encoded with.
 
     Args:
-        circuit (object): Qiskit circuit object to decode.
-        **kwargs: Additional keyword arguments passed to the decoding method.
+        circuit: Qiskit circuit object to decode.
+        **kwargs: Additional keyword arguments passed to the decoding method. Refer to the scheme's `decode` method.
 
     Returns:
         Decoded data from the quantum circuit.
@@ -35,12 +36,12 @@ def decode(circuit, **kwargs):
 
 # ------------------- Tool Function ---------------------------
 
-def stream(data, scheme="qpam", **kwargs):
+def stream(data: 'np.ndarray', scheme: Union[str, quantumaudio.schemes.Scheme] = "qpam", **kwargs):
     """Streams data through a quantum encoding scheme for longer arrays.
 
     Args:
-        data (any): Data to be streamed.
-        scheme (str): Name of the quantum scheme to use for streaming.
+        data: Data to be streamed.
+        scheme: Name of the quantum scheme to use for streaming.
         **kwargs: Additional keyword arguments passed to the streaming method.
 
     Returns:
@@ -53,22 +54,22 @@ def stream(data, scheme="qpam", **kwargs):
 
 # ------------------- Additional Functions ---------------------------
 
-def calculate(data, scheme="qpam", **kwargs):
+def calculate(data: 'np.ndarray', scheme: Union[str, quantumaudio.schemes.Scheme] = "qpam", **kwargs):
     """Estimates and Prints the resources required (number of qubits) according to a scheme.
 
     Args:
-        data (any): The data to encode.
-        scheme (str): Name of the encoding scheme to use. Defaults to "qpam".
+        data: The data to encode.
+        scheme: Name of the encoding scheme or a scheme object to use. Defaults to "qpam".
         **kwargs: Additional keyword arguments passed to the scheme class.
     """
     _load_scheme(scheme, **kwargs).calculate(data)
 
-def decode_result(result, **kwargs):
+def decode_result(result: Union['qiskit.result.Result','qiskit.primitives.PrimitiveResult','qiskit.primitives.SamplerPubResult'], **kwargs):
     """Decodes a quantum result object.
 
     Args:
-        result (object): Qiskit result object to decode.
-        **kwargs: Additional keyword arguments passed to the decoding method.
+        result: Qiskit result object to decode.
+        **kwargs: Additional keyword arguments passed to the decoding method. Refer to the scheme's `decode_result` method.
 
     Returns:
         Decoded data from the result object.
@@ -77,13 +78,13 @@ def decode_result(result, **kwargs):
     return _load_scheme(scheme, **scheme_kwargs).decode_result(result, **kwargs)
 
 
-def decode_counts(counts, metadata, **kwargs):
-    """Decodes quantum counts using metadata.
+def decode_counts(counts: dict, metadata: dict, **kwargs):
+    """Decodes Quantum circuit measurement counts using metadata.
 
     Args:
-        counts (dict): Quantum counts to decode.
-        metadata (dict): Metadata associated with the circuit that was executed.
-        **kwargs: Additional keyword arguments passed to the decoding method.
+        counts: Counts dictionary to decode.
+        metadata: Metadata dictionary associated with the circuit that was executed.
+        **kwargs: Additional keyword arguments passed to the decoding method. Refer to the scheme's `decode_counts` method.
 
     Returns:
         Decoded data from the counts.
@@ -97,13 +98,13 @@ def decode_counts(counts, metadata, **kwargs):
 
 _cache = {}
 
-def _load_scheme(scheme, **scheme_kwargs):
+def _load_scheme(scheme: Union[str, quantumaudio.schemes.Scheme], **scheme_kwargs):
     """Load a scheme with specified keyword arguments, caching the result to 
     avoid repeated loading for the same parameters.
 
     Args:
-        scheme (str): Name of the quantum scheme to use for streaming.
-        scheme_kwargs (dict): Dictionary containing keyword arguments.
+        scheme: Name of the quantum scheme or scheme object to use for streaming.
+        scheme_kwargs: Dictionary containing keyword arguments.
 
     Returns:
         The loaded or cached scheme object.
@@ -116,7 +117,7 @@ def _load_scheme(scheme, **scheme_kwargs):
         _cache[cache_key] = load_scheme(scheme, **scheme_kwargs)
     return _cache[cache_key]
 
-def _split_kwargs(kwargs):
+def _split_kwargs(kwargs: dict):
     """Splits keyword arguments between scheme loading and encoding operations.
 
     Args:
@@ -131,7 +132,7 @@ def _split_kwargs(kwargs):
     }, kwargs
 
 
-def _fetch_kwargs(instance, kwargs):
+def _fetch_kwargs(instance: Union['dict','qiskit.QuantumCircuit','qiskit.result.Result','qiskit.primitives.PrimitiveResult','qiskit.primitives.SamplerPubResult'], kwargs: dict):
     """Fetches scheme at decoding and splits keyword arguments accordingly.
 
     Args:
