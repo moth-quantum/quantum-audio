d036334483e1fd62c1ac85426418b67b7430c6df
hari <74590303+stringtherapy@users.noreply.github.com>
2024-10-05 22:12:32 +0100

    Update __init__.py

--- quantumaudio/utils/__init__.py
+++ quantumaudio/utils/__init__.py
@@ -15,10 +15,12 @@
 
 """This subpackage contains the following utility modules that support the encoding and decoding schemes:
 
-- **data.py**: Data preparation and calculation functions.
-- **convert.py**: Data pre-processing functions required for encoding values into the quantum circuit.
 - **circuit.py**: Helper functions for quantum audio circuit preparations with Qiskit.
-- **qiskit_helpers.py**: Common helper functions for obtaining circuit results and preview information on the circuit. It includes Aer Simulator as the default backend if no backend is specified.
+- **convert.py**: Data pre-processing functions required for encoding values into the quantum circuit.
+- **data.py**: Data preparation and calculation functions.
+- **execute.py**: Helper Functions for executing circuits. Uses AerSimulator as Default backend.
+- **preview.py**: Functions to draw and print information of a circuit.
+- **results.py**: Common helper functions for obtaining circuit results.
 """
 
 from .circuit import *
