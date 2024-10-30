c04e7cfb6436663e1f2801e03a72517bf63809a5
Vignesh S <74590303+stringtherapy@users.noreply.github.com>
2024-10-30 14:22:11 +0000

    docs-arranging
    kept a copy of docs from previous version for readthedocs version fixing

--- docs/conf.py
+++ docs/conf.py
@@ -3,31 +3,33 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 import os
 import sys
 
 sys.path.insert(0, os.path.abspath(".."))
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
-project = 'quantumaudio'
-copyright = '2022, Paulo Vitor Itaboraí, ICCMR'
-author = 'Paulo Vitor Itaboraí'
-release = '0.0.2'
-version = '0.0.2rc2'
+project = 'Quantum Audio'
+copyright = '2024, Moth Quantum'
+author = 'Moth Quantum'
+release = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.autodoc", "sphinx.ext.napoleon", "nbsphinx", "nbsphinx_link", "sphinx.ext.mathjax", "IPython.sphinxext.ipython_console_highlighting"]
-
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+]
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-# remova package name
-add_module_names = False
+autodoc_member_order = "bysource"
+#autoclass_content = 'init' #show __init__ attributes
+autodoc_typehints = 'description' 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
