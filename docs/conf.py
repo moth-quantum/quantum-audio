54e40c078e084186213824368d56d974dd8895d8
Vignesh S <74590303+stringtherapy@users.noreply.github.com>
2025-04-16 14:11:44 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath(".."))
 project = 'Quantum Audio'
 copyright = '2024, Moth Quantum'
 author = 'Moth Quantum and collaborators'
-release = '0.1.0'
+release = '0.2.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
